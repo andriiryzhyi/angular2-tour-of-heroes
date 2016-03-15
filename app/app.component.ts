62dc024a65f17aa1bff026d3925617fb94443c6a
Andrii Ryzhyi <andrii.ryzhyi@gmail.com>
2016-03-15 19:20:04 +0200

    Configure a Route with a Parameter

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,6 +4,8 @@ import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/route
 import { HeroService }     from './hero.service';
 import { HeroesComponent } from './heroes.component';
 import { DashboardComponent } from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
+
 
 @Component({
   selector: 'my-app',
@@ -33,7 +35,12 @@ import { DashboardComponent } from './dashboard.component';
     path: '/heroes',
     name: 'Heroes',
     component: HeroesComponent
-  }
+  },
+  {
+    path: '/detail/:id',
+    name: 'HeroDetail',
+    component: HeroDetailComponent
+  },
 ])
 
 export class AppComponent {
