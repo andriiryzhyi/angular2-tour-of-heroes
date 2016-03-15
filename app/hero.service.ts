1be4f34d6ba139df0c9f614a6c0951f086bb9ca2
Andrii Ryzhyi <andrii.ryzhyi@gmail.com>
2016-03-15 17:00:39 +0200

    Added method getHeroesSlowly()

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,3 +1,4 @@
+// import {Hero} from './hero';
 import {HEROES} from './mock-heroes';
 import {Injectable} from 'angular2/core';
 
@@ -6,4 +7,10 @@ export class HeroService {
   getHeroes() {
     return Promise.resolve(HEROES);
   }
+  // See the "Take it slow" appendix
+  // getHeroesSlowly() {
+  //   return new Promise<Hero[]>(resolve =>
+  //     setTimeout(()=>resolve(HEROES), 2000) // 2 seconds
+  //   );
+  // }
 }
