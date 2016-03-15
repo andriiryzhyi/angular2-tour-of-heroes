a7b414f2d72c21bcb119e121660d16103ef032f3
Andrii Ryzhyi <andrii.ryzhyi@gmail.com>
2016-03-15 19:41:28 +0200

    Select a Dashboard Hero

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,7 +6,6 @@ import { HeroesComponent } from './heroes.component';
 import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 
-
 @Component({
   selector: 'my-app',
   template: `
