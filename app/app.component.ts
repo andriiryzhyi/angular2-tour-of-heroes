11a1262dc437211af362c03b90bc08131f793c8e
Andrii Ryzhyi <andrii.ryzhyi@gmail.com>
2016-03-15 13:40:31 +0200

    Change a Hero template

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,7 +7,15 @@ interface Hero {
 
 @Component({
     selector: 'my-app',
-    template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>'
+    template:`
+      <h1>{{title}}</h1>
+      <h2>{{hero.name}} details!</h2>
+      <div><label>id: </label>{{hero.id}}</div>
+      <div>
+        <label>name: </label>
+        <div><input value="{{hero.name}}" placeholder="name"></div>
+      </div>
+    `
 })
 
 export class AppComponent {
