c95840867fef9b5875cabae84902f0dcf02fbf95
Andrii Ryzhyi <andrii.ryzhyi@gmail.com>
2016-03-15 16:29:57 +0200

    Act on the Promise

--- app/app.component.ts
+++ app/app.component.ts
@@ -82,7 +82,7 @@ export class AppComponent implements OnInit {
   }
 
   getHeroes() {
-    this.heroes = this._heroService.getHeroes();
+    this._heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
 
   ngOnInit() {
