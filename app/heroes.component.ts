3789ee314329a996d5c6f210efdf1a3d1a1db02a
Andrii Ryzhyi <andrii.ryzhyi@gmail.com>
2016-03-15 17:23:26 +0200

    Deleted providers section from HeroesComponent

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -66,8 +66,7 @@ import {HeroService} from './hero.service';
       border-radius: 4px 0 0 4px;
     }
   `],
-  directives: [HeroDetailComponent],
-  providers: [HeroService]
+  directives: [HeroDetailComponent]
 })
 
 export class HeroesComponent implements OnInit {
